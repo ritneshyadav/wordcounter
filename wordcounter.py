625939bae756d98991498dae75f43632c6413a9b
ritneshyadav <76439338+ritneshyadav@users.noreply.github.com>
2021-10-17 13:24:56 +0530

    Add files via upload

---
+++ wordcounter.py
@@ -0,0 +1,3 @@
+print("Welcome to the word counter software by Chirantan Banerjee")
+a = input("Type the article you want to count the words : ")
+print("Number of words in these string is:",len(a))
