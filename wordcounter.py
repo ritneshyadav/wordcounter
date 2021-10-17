a940823d778f5fbb615b925f078cec7653339eb4
Chirantan Banerjee <65519645+ChirantanTech@users.noreply.github.com>
2021-10-17 13:30:14 +0530

    Update Word Counter
    Some Desc added to the file.Although its not a advance project.

--- wordcounter.py
+++ wordcounter.py
@@ -1,3 +1,6 @@
-print("Welcome to the word counter software by Chirantan Banerjee")
+print("Welcome to the word counter software.")
 a = input("Type the article you want to count the words : ")
 print("Number of words in these string is:",len(a))
+
+#Desc
+A simple beginner project created in python.
